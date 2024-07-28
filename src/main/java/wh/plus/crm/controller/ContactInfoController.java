5af2e9d33f9273ee36058228789ca568d9f72a01
marcin690 <35170815+marcin690@users.noreply.github.com>
2024-07-28 18:07:31 +0200

    Dodanie DTO na ContactInfo, przed rozwiaznaiem problmu z auditorrm, który powoduje błędy z put na contactinfo

--- src/main/java/wh/plus/crm/controller/ContactInfoController.java
+++ src/main/java/wh/plus/crm/controller/ContactInfoController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import wh.plus.crm.dto.ContactInfoDTO;
 import wh.plus.crm.model.contactInfo.ContactInfo;
 import wh.plus.crm.repository.ContactInfoRepository;
 import wh.plus.crm.service.ContactInfoService;
@@ -21,36 +22,36 @@ public class ContactInfoController {
     @Autowired
     private ContactInfoService contactInfoService;
 
-    @GetMapping
-    public ResponseEntity<List<ContactInfo>> findAllContactInfo() {
-        List<ContactInfo> contactInfoList = contactInfoService.findAll();
-        return new ResponseEntity<>(contactInfoList, HttpStatus.OK);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ContactInfo> getContactInfoById(@PathVariable Long id) {
-        Optional<ContactInfo> contactinfoOptional = contactInfoService.findById(id);
-        return contactinfoOptional.map(
-                value -> new ResponseEntity<>(value, HttpStatus.OK)
-        ).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
     @PostMapping
-    public ResponseEntity<ContactInfo> createContactInfo(@RequestBody ContactInfo contactInfo, Authentication authentication){
-        String username = authentication.getName();
-        ContactInfo createdContactInfo = contactInfoService.save(contactInfo);
-        return new ResponseEntity<>(createdContactInfo, HttpStatus.CREATED);
+    public ResponseEntity<ContactInfoDTO> createContactInfo(@RequestBody ContactInfoDTO contactInfoDTO) {
+        ContactInfoDTO createdContactInfo = contactInfoService.createContactInfo(contactInfoDTO);
+        return ResponseEntity.ok(createdContactInfo);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ContactInfo> updateContactInfo(@PathVariable Long id, @RequestBody ContactInfo contactInfo) {
-        ContactInfo updatedContactInfo = contactInfoService.update(id, contactInfo);
-        if (updatedContactInfo != null) {
-            return new ResponseEntity<>(updatedContactInfo, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<ContactInfoDTO> updateContactInfo(@PathVariable Long id, ContactInfoDTO contactInfoDTO) {
+        ContactInfoDTO updatedContactInfo = contactInfoService.updateContactInfo(id, contactInfoDTO);
+        return ResponseEntity.ok(updatedContactInfo);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContactInfoDTO> getContactInfo(@PathVariable Long id) {
+        ContactInfoDTO contactInfoDTO = contactInfoService.getContactInfo(id);
+        return ResponseEntity.ok(contactInfoDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity <List<ContactInfoDTO>> getAllContactInfos() {
+        List<ContactInfoDTO> contactInfosDTO = contactInfoService.getAllContactInfos();
+        return ResponseEntity.ok(contactInfosDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteContactInfo(@PathVariable Long id){
+        contactInfoService.deleteContactInfo(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 
--- src/main/java/wh/plus/crm/service/ContactInfoService.java
+++ src/main/java/wh/plus/crm/service/ContactInfoService.java
@@ -1,12 +1,16 @@
 package wh.plus.crm.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import wh.plus.crm.dto.ContactInfoDTO;
+import wh.plus.crm.mapper.ContactInfoMapper;
 import wh.plus.crm.model.contactInfo.ContactInfo;
 import wh.plus.crm.repository.ContactInfoRepository;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class ContactInfoService {
@@ -14,26 +18,45 @@ public class ContactInfoService {
     @Autowired
     private ContactInfoRepository contactInfoRepository;
 
-   public List<ContactInfo> findAll() {
-       return contactInfoRepository.findAll();
-   }
+    @Autowired
+    private ContactInfoMapper contactInfoMapper;
 
-   public Optional<ContactInfo> findById(Long id) {
-       return contactInfoRepository.findById(id);
-   }
-
-   public ContactInfo save(ContactInfo contactInfo) {
-     return contactInfoRepository.save(contactInfo);
-   }
-
-    public ContactInfo update(Long id, ContactInfo contactInfo) {
-        if (contactInfoRepository.existsById(id)) {
-            contactInfo.setId(id);
-            return contactInfoRepository.save(contactInfo);
-        } else {
-            return null;
-        }
+    @Transactional
+    public ContactInfoDTO createContactInfo(ContactInfoDTO contactInfoDTO) {
+        ContactInfo contactInfo = contactInfoMapper.toEntity(contactInfoDTO);
+        ContactInfo savedContactInfo = contactInfoRepository.save(contactInfo);
+        return contactInfoMapper.toDTO(savedContactInfo);
     }
 
+    @Transactional
+    public ContactInfoDTO updateContactInfo(Long id, ContactInfoDTO contactInfoDTO){
+        ContactInfo contactInfo = contactInfoRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Contact info not found"));
 
+        contactInfoMapper.updateEntity(contactInfoDTO, contactInfo); // Poprawiona linia
+        ContactInfo updatedContactInfo = contactInfoRepository.save(contactInfo);
+        return contactInfoMapper.toDTO(updatedContactInfo);
+    }
+
+    @Transactional
+    public ContactInfoDTO getContactInfo(Long id) {
+        ContactInfo contactInfo = contactInfoRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Contact Info not found"));
+        return contactInfoMapper.toDTO(contactInfo);
+    }
+
+    @Transactional
+    public List<ContactInfoDTO> getAllContactInfos() {
+        List<ContactInfo> contactInfos = contactInfoRepository.findAll();
+        return contactInfos.stream()
+                .map(contactInfoMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void deleteContactInfo(Long id){
+        ContactInfo contactInfo = contactInfoRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Contact Info not found"));
+        contactInfoRepository.delete(contactInfo);
+    }
 }
