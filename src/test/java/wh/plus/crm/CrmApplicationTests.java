0b865c0fde63715fa3f9c51469b927b00ffaca35
marcin690 <35170815+marcin690@users.noreply.github.com>
2024-12-21 20:24:09 +0100

    Gotowa aplikajca

--- src/test/java/wh/plus/crm/CrmApplicationTests.java
+++ src/test/java/wh/plus/crm/CrmApplicationTests.java
@@ -1,13 +1,14 @@
 package wh.plus.crm;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CrmApplicationTests {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApplication.class, args);
     }
-
 }
