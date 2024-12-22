6c303b395dd2d0342be8b6a46c2c23bb7b9d44e8
marcin690 <35170815+marcin690@users.noreply.github.com>
2024-12-22 21:46:22 +0100

    Zmiany w aplication dev proporties

--- src/main/java/wh/plus/crm/model/user/User.java
+++ src/main/java/wh/plus/crm/model/user/User.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 @Entity
 @Table(name = "users")
 @Data
+
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
