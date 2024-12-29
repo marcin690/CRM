d82e4062efc35288c3ac4d0eac7b27c8299ac60d
marcin690 <35170815+marcin690@users.noreply.github.com>
2024-12-29 17:11:35 +0100

    Dodana wyszukiwarka klientów

---
+++ src/main/java/wh/plus/crm/specyfications/ClientSpecification.java
@@ -0,0 +1,43 @@
+package wh.plus.crm.specyfications;
+
+import org.springframework.data.jpa.domain.Specification;
+import wh.plus.crm.model.client.Client;
+
+public class ClientSpecification {
+
+    public static Specification<Client> hasClientType(String clientType) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("clientType"), clientType);
+    }
+
+    public static Specification<Client> hasClientFullName(String fullName){
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(
+                        criteriaBuilder.lower(root.get("clientFullName")),
+                        "%" + fullName.toLowerCase() + "%"
+                );
+    }
+
+
+    public static Specification<Client> hasClientBusinessName(String businessName){
+        return (root, query, criteriaBuilder) ->  criteriaBuilder.equal(root.get("clientBusinessName"), businessName);
+    }
+
+    public static Specification<Client> hasClientEmail(String email) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(criteriaBuilder.lower(root.get("clientEmail")), "%" + email.toLowerCase() + "%");
+    }
+
+    public static Specification<Client> hasClientPhone(Long phone) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("clientPhone"), phone);
+    }
+
+    public static Specification<Client> hasVatNumber(Long vatNumber) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("vatNumber"), vatNumber);
+    }
+
+
+
+
+}
